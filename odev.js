2df019fb9e8ad9532c3051f8f8ea316425df6ab4
Emre Sabancı <ecsabancii@gmail.com>
2020-12-09 15:55:21 +0000

    Ödev tamamlandı.

--- odev.js
+++ odev.js
@@ -5,12 +5,12 @@
 
 var person = {
   name: "John",
-  message: function () {    
+  message: function () {
     console.log("Hello, " + this.name)
   }
 }
 
-var messageFunc = person.message
+var messageFunc = person.message.bind(person)
 messageFunc();
 
 
@@ -23,12 +23,12 @@ messageFunc();
   sonuclarini yazdiracak sekilde duzenleyiniz.
 */
 var numbers = {
-  numbers: [[10,20,30], 2],
-  multiply: function(){
-    this.numbers[0].map(function(number, numberIndex){
-        const result = number * this.numbers[1];
-        console.log(result)
-    })
+  numbers: [[10, 20, 30], 2],
+  multiply: function () {
+    this.numbers[0].map(function (number, numberIndex) {
+      const result = number * this.numbers[1];
+      console.log(result)
+    }.bind(this))
   }
 };
 
@@ -42,8 +42,19 @@ numbers.multiply();
   Ornek : isValidName("John") true donmeli
   Ornek : isValidName(" J ohn") false donmeli
 */
-function isValidName(name){
-
+function isValidName(name) {
+  // girilen değerin number olup olmadığı kontrol ediliyor. Number ise direkt false dönüyor..
+  if(!isNaN(Number(name))) {
+    return false;
+  }
+  // girilen değer number değil string ise burada gereksiz boşluk olup olmadığı kontrol ediliyor.
+  else if (name.includes(" ")) {
+    return false;
+  }
+  // girilen değer string ve boşluk yoksa direkt true return ediliyor.
+  else {
+    return true;
+  }
 }
 
 /*
@@ -58,8 +69,15 @@ function isValidName(name){
   Ornek: katilimSaati("3", 20) 60 sonucunu vermelidir.
   Ornek: katilimSaati("5", "30") 150 sonucunu vermelidir.
 */
-function katilimSaati(dersSayisi, dersSuresi){
-
+function katilimSaati(dersSayisi, dersSuresi) {
+  let product = Number(dersSayisi) * Number(dersSuresi);
+  // çarpım sonucu elde edilen değer NaN ise if bloğuna giriliyor.
+  if(isNaN(product)){
+    console.log("Please try to enter valid parameters....")
+  }
+  else {
+    console.log(product)
+  }
 }
 
 
